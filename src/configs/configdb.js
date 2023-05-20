dbad14ecdb4014e6690da3e618c1c7fd467a8f7f
gaivits <49124734+gaivits@users.noreply.github.com>
2023-05-20 16:31:37 +0700

    Add files via upload

--- src/configs/configdb.js
+++ src/configs/configdb.js
@@ -1,11 +1,11 @@
 const mssql = require("mssql");
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 
 const sqlConfig = {
   user: process.env.DB_USER,
-  password: process.env.DB_PWD ,
+  password: process.env.DB_PWD,
   database: process.env.DB_NAME,
-  port:parseInt(process.env.DB_PORT),
+  port: parseInt(process.env.DB_PORT),
   server: process.env.DB_HOST,
   pool: {
     max: 10,
